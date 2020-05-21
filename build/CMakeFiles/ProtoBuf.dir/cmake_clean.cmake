49861e1f2c6d7a97d28ff358a9c3577c7a81f180
yankai <Mediumcore@1263910275@qq.com>
2020-05-21 11:30:24 +0800

    protobuf cmake

---
+++ build/CMakeFiles/ProtoBuf.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "DeviceMessage.pb.cc"
+  "DeviceMessage.pb.h"
+  "CMakeFiles/ProtoBuf.dir/main.cpp.o"
+  "CMakeFiles/ProtoBuf.dir/DeviceMessage.pb.cc.o"
+  "ProtoBuf.pdb"
+  "ProtoBuf"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProtoBuf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
